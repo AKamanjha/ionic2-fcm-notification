85abadc17cf6e054297a2f7c18b56a86252d8afe
Aman Jha <ak.amanjha@gmail.com>
2017-06-25 14:32:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FCM } from '@ionic-native/fcm';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -9,14 +10,34 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private fcm: FCM) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      
+
+      fcm.getToken().then(token => {
+        alert("Token : "+ token);
+      })
+
+      fcm.onNotification().subscribe(data => {
+        if (data.wasTapped) {
+          alert('received in background')
+        } else {
+          alert("Received in foreground");
+        };
+      })
+
+      fcm.onTokenRefresh().subscribe(token => {
+        alert("token Refresh : "+token);
+      })
+
+      //fcm.unsubscribeFromTopic('marketing');
     });
   }
 }
